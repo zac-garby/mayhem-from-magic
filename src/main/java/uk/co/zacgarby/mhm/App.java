1c50f9fd36f454c517ff0d488e4795cf0a200735
Zac Garby <me@zacgarby.co.uk>
2020-04-24 19:34:46 +0100

    Draw the sidebar background

--- src/main/java/uk/co/zacgarby/mhm/App.java
+++ src/main/java/uk/co/zacgarby/mhm/App.java
@@ -8,23 +8,19 @@ import uk.co.zacgarby.mhm.engine.Window;
 
 public class App implements Game {
 	private Batch batch, lightBatch;
-	private Texture sun, moon;
+	private Texture sidebar;
 	private float x = 0;
 	private Framebuffer lightmap;
 	
 	@Override
 	public void setup() {
-		sun = new Texture("resources/textures/sun.png");
-		moon = new Texture("resources/textures/moon.png");
+		sidebar = new Texture("resources/textures/sidebar.png");
 		
-		batch = new Batch(512);
-		batch.register(sun);
-		batch.register(moon);
+		batch = new Batch(256);
+		batch.register(sidebar);
 		batch.createAtlas();
 		
-		lightBatch = new Batch(512);
-		lightBatch.register(sun);
-		lightBatch.register(moon);
+		lightBatch = new Batch(256);
 		lightBatch.createAtlas();
 		
 		lightmap = new Framebuffer(250, 200);
@@ -44,15 +40,11 @@ public class App implements Game {
 	public void render() {
 		lightmap.bind();
 		lightBatch.start();
-		lightBatch.draw(moon, 125 - 32, 100 - 32, 64, 64);
 		lightBatch.flush();
 		lightmap.unbind();
 		
 		batch.start();
-		batch.draw(moon, 125 + (float) (80 * Math.sin(x * 1.643533)), 100 + (float) (50 * Math.sin(x * 1.231235)), 32, 32);
-		batch.draw(moon, 125 + (float) (80 * Math.sin(15 + x * -1.243533)), 100 + (float) (50 * Math.sin(5 + x * 1.931235)), 32, 32);
-		batch.draw(moon, 125 + (float) (80 * Math.sin(30 + x * 1.743533)), 100 + (float) (50 * Math.sin(10 + x * -2.331235)), 32, 32);
-		batch.draw(sun, 125 - 32, 100 - 32, 64, 64);
+		batch.draw(sidebar, 170, 0);
 		batch.flush();
 	}
 	
